c0b06d55577de0d039a7f91eed36695013011814
frkam <frkam@icloud.com>
2023-04-20 19:26:48 +0300

    fix(pages/home): delete unnecessary form from home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,10 @@
 import { styled } from '@/shared/config/stitches/stitches.config'
-import { Form, Input, Text } from '@/shared/ui'
+import { Text } from '@/shared/ui'
 
 export const HomePage = () => {
   return (
     <Section>
       <Text variant="h1">Home Page</Text>
-      <Form>
-        <Form.Item label="User">
-          <Input />
-        </Form.Item>
-      </Form>
     </Section>
   )
 }
