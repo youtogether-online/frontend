7f66a26aa4334be466c65be92f82b0df7bdbdb22
frkam <frkam@icloud.com>
2023-05-13 01:07:35 +0300

    chore: add ssl for development mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import * as path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
@@ -16,15 +17,16 @@ export default defineConfig({
         plugins: ['effector/babel-plugin'],
       },
     }),
+    mkcert(),
   ],
   server: {
     hmr: false,
     watch: {
       usePolling: true,
     },
-    host: true,
     strictPort: true,
     port: 5173,
+    https: true,
   },
   preview: {
     port: 5173,
