8768941a788be0efae80635b6a6ccf3ad5a97eeb
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-05-23 01:10:10 +0300

    chore: update eslint and prettier | fix eol

--- src/shared/ui/typography/text/text.tsx
+++ src/shared/ui/typography/text/text.tsx
@@ -4,7 +4,7 @@ import { type ReactNode } from "react";
 import { type SxProp } from "../../types";
 
 type TextProps = {
-  asChild: boolean;
+  asChild?: boolean;
   children: ReactNode;
 } & SxProp;
 
