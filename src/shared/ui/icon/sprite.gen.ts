db738c16ec2870737d6d343e8c08d2877b9e1fb2
frkam <gearboxop@gmail.com>
2023-06-09 17:26:01 +0300

    chore(translations): add translation for not found page

--- src/shared/ui/icon/sprite.gen.ts
+++
@@ -1,9 +0,0 @@
-export interface SpritesMap {
-  logos: "youtogether-horizontal" | "youtogether-vertical";
-  common: "arrow-left";
-}
-
-export const SPRITES_META: { [K in keyof SpritesMap]: SpritesMap[K][] } = {
-  logos: ["youtogether-horizontal", "youtogether-vertical"],
-  common: ["arrow-left"],
-};
--- src/shared/ui/icon/sprites.gen.ts
+++ src/shared/ui/icon/sprites.gen.ts
@@ -3,7 +3,7 @@ export interface SpritesMap {
   logos: "youtogether-horizontal" | "youtogether-vertical";
 }
 
-export const SPRITES_META: { [K in keyof SpritesMap]: Array<SpritesMap[K]> } = {
+export const SPRITES_META: { [K in keyof SpritesMap]: SpritesMap[K][] } = {
   common: ["arrow-left"],
   logos: ["youtogether-horizontal", "youtogether-vertical"],
 };
